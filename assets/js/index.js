997bccc9298ff193359df34b4568c2108ac3b7ef
Sergio Gómez <sergioagmoreno@gmail.com>
2024-08-10 01:29:45 -0500

    more progress on mobile menu, footer and some js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,18 @@
 const menuButton = document.getElementById('menuButton');
+const primaryNav = document.querySelector('.primaryNav');
+const year = document.getElementById('year');
+const backToTop = document.getElementById('backToTop');
 
 menuButton.addEventListener('click', () => {
     menuButton.classList.toggle('menuOpen');
-})
+    primaryNav.classList.toggle('menuOpen');
+});
+
+year.innerText = new Date().getFullYear();
+
+backToTop.addEventListener('click', () => {
+    document.querySelector('#mainHeader').scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
